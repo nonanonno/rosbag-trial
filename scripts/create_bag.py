d23512e065098a45390acddd053ae260c64066a7
nonanonno <as.nonanonno@gmail.com>
2022-12-04 11:50:30 +0000

    add geometry_msgs/msg/Point

--- scripts/create_bag.py
+++ scripts/create_bag.py
@@ -2,6 +2,7 @@
 
 import os
 import shutil
+from math import sin, cos
 
 import rosbag2_py
 from rclpy.serialization import serialize_message
@@ -13,6 +14,9 @@ from dataclasses import dataclass
 from typing import Tuple, List
 
 from std_msgs.msg import Int32, String
+from geometry_msgs.msg import Point
+from sensor_msgs.msg import Image
+
 from cv_bridge import CvBridge
 
 
@@ -40,12 +44,17 @@ def to_timestamp(milli: int):
     return milli * 1000 * 1000
 
 
-def create_int32_bag(bag_path: str):
+def create_new_writer(bag_path: str) -> rosbag2_py.SequentialWriter:
     if os.path.exists(bag_path):
         shutil.rmtree(bag_path)
 
     writer = rosbag2_py.SequentialWriter()
     writer.open(*get_rosbag_options(bag_path))
+    return writer
+
+
+def create_int32_bag(bag_path: str):
+    writer = create_new_writer(bag_path)
 
     topic_name = "/number"
     create_topic(writer, topic_name, "std_msgs/msg/Int32")
@@ -56,11 +65,7 @@ def create_int32_bag(bag_path: str):
 
 
 def create_string_bag(bag_path: str):
-    if os.path.exists(bag_path):
-        shutil.rmtree(bag_path)
-
-    writer = rosbag2_py.SequentialWriter()
-    writer.open(*get_rosbag_options(bag_path))
+    writer = create_new_writer(bag_path)
 
     topic_name = "/chatter"
     create_topic(writer, topic_name, "std_msgs/msg/String")
@@ -70,15 +75,31 @@ def create_string_bag(bag_path: str):
         writer.write(topic_name, serialize_message(msg), to_timestamp(i * 1000))
 
 
+def create_point_bag(bag_path: str):
+    writer = create_new_writer(bag_path)
+
+    dt = 0.1
+    vx = 5
+
+    topic_name = "/point"
+    create_topic(writer, topic_name, "geometry_msgs/msg/Point")
+
+    for i in range(100):
+        x = i * dt * vx
+        y = sin(x)
+        z = cos(x)
+
+        msg = Point(x=x, y=y, z=z)
+        writer.write(
+            topic_name, serialize_message(msg), to_timestamp(int(i * dt * 1000))
+        )
+
+
 def create_video_bag(bag_path: str):
-    if os.path.exists(bag_path):
-        shutil.rmtree(bag_path)
+    writer = create_new_writer(bag_path)
 
     dt = 0.1
     anim = Animation(dt)
-
-    writer = rosbag2_py.SequentialWriter()
-    writer.open(*get_rosbag_options(bag_path))
 
     topic_name = "/image"
     create_topic(writer, topic_name, "sensor_msgs/msg/Image")
@@ -101,10 +122,16 @@ def create_mix_bag(bag_path: str):
     writer = rosbag2_py.SequentialWriter()
     writer.open(*get_rosbag_options(bag_path))
 
+    def wave_point(i):
+        vx = 5
+        x = i * 0.001 * vx
+        return Point(x=x, y=sin(x), z=cos(x))
+
     # topic, type, dt, msg callback
     topics = [
         ("/number", "std_msgs/msg/Int32", 0.5, lambda i: Int32(data=i)),
         ("/chatter", "std_msgs/msg/String", 1, lambda i: String(data=f"Hello {i}")),
+        ("/point", "geometry_msgs/msg/Point", 0.1, lambda i: wave_point(i)),
         ("/image", "sensor_msgs/msg/Image", 0.1, lambda _: anim.get_and_update()),
     ]
 
@@ -112,18 +139,22 @@ def create_mix_bag(bag_path: str):
         create_topic(writer, topic[0], topic[1])
 
     for milli in range(10000):
+        # https://github.com/ros-visualization/rqt_bag/issues/136
+        offset = 0
         for topic in topics:
             target_milli = int(topic[2] * 1000)
             if milli % target_milli != 0:
                 continue
             msg = topic[3](milli)
-            writer.write(topic[0], serialize_message(msg), to_timestamp(milli))
+            writer.write(topic[0], serialize_message(msg), to_timestamp(milli + offset))
+            offset += 1
 
 
 def main():
     data_dir = os.path.join(os.path.dirname(__file__), "..", "data")
     create_int32_bag(os.path.join(data_dir, "int32.bag"))
     create_string_bag(os.path.join(data_dir, "string.bag"))
+    create_point_bag(os.path.join(data_dir, "point.bag"))
     create_video_bag(os.path.join(data_dir, "video.bag"))
     create_mix_bag(os.path.join(data_dir, "mix.bag"))
 
@@ -188,7 +219,7 @@ class Animation:
 
         self.bridge = CvBridge()
 
-    def get_and_update(self):
+    def get_and_update(self) -> Image:
         img = np.zeros((self.height, self.width, 3), np.uint8)
         self.field.draw(img)
         cv_img = self.bridge.cv2_to_imgmsg(img, "bgr8")
